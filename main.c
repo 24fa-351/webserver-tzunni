c1f686c8e4b61f282217484fa75401b60e69f3f8
Keith Bui <kqbui@csu.fullerton.edu>
2024-11-12 01:52:11 -0800

    adapted echo to use html, then separated main into function files (like i should have last assignment tbh)

--- main.c
+++ main.c
@@ -6,57 +6,39 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <dirent.h>
 
-#define PORT 50001
+#include "function.h"
+
+#define DEFAULT_PORT 80
 #define LISTEN_BACKLOG 5
 
-void* handleConnection(void* client_fd_ptr)
+int main(int argc, char *argv[])
 {
-  int client_fd = *(int*)client_fd_ptr;
-  free(client_fd_ptr);
-
-  char buffer[1024];
-  int bytesRead;
-
-  while ((bytesRead = read(client_fd, buffer, sizeof(buffer))) > 0)
+  int port = DEFAULT_PORT;
+  if (argc == 3 && strcmp(argv[1], "-p") == 0)
   {
-    buffer[bytesRead] = '\0'; // Null-terminate the buffer
-    printf("Received: %s\n", buffer);
-    write(client_fd, buffer, bytesRead);
+    port = atoi(argv[2]);
   }
 
-  close(client_fd);
-  return NULL;
-}
-
-int main(int argc, char* argv[])
-{
   int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-
   struct sockaddr_in socket_address;
-  memset(&socket_address, '\0', sizeof(socket_address));
+  memset(&socket_address, 0, sizeof(socket_address));
   socket_address.sin_family = AF_INET;
   socket_address.sin_addr.s_addr = htonl(INADDR_ANY);
-  socket_address.sin_port = htons(PORT);
+  socket_address.sin_port = htons(port);
 
-  int returnval;
-
-  returnval = bind(
-      socket_fd, (struct sockaddr *)&socket_address, sizeof(socket_address));
-
-  returnval = listen(socket_fd, LISTEN_BACKLOG);
-
-  struct sockaddr_in client_address;
-  socklen_t client_address_len = sizeof(client_address);
+  bind(socket_fd, (struct sockaddr *)&socket_address, sizeof(socket_address));
+  listen(socket_fd, LISTEN_BACKLOG);
 
   while (1)
   {
-    int* client_fd_ptr = malloc(sizeof(int));
-    *client_fd_ptr = accept(socket_fd, (struct sockaddr *)&client_address, &client_address_len);
-
-    pthread_t thread_id;
-    pthread_create(&thread_id, NULL, handleConnection, client_fd_ptr);
-    pthread_detach(thread_id);
+    int client_fd = accept(socket_fd, NULL, NULL);
+    int *client_fd_ptr = malloc(sizeof(int));
+    *client_fd_ptr = client_fd;
+    pthread_t thread;
+    pthread_create(&thread, NULL, handle_connection, client_fd_ptr);
+    pthread_detach(thread);
   }
 
   close(socket_fd);
