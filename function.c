75625dee2bca48d1d8694d2d7a74ca065ce7dd9b
Keith Bui <kqbui@csu.fullerton.edu>
2024-11-12 02:04:01 -0800

    trimmed some functions

--- function.c
+++ function.c
@@ -1,13 +1,11 @@
-#include "function.h"
-#include <arpa/inet.h>
-#include <netinet/in.h>
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
-#include <dirent.h>
+
+#include "function.h"
 
 #define STATIC_DIR "./static"
 
--- main.c
+++ main.c
@@ -6,7 +6,6 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
-#include <dirent.h>
 
 #include "function.h"
 
